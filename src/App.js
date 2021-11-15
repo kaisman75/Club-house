ff01e7258ca042cea29f26d08ba7fa7a21fb0be4
kais manai <kaismanai1975@gmail.com>
2021-11-15 11:58:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Welcom from './pages/Welcom';
+
+function App() {
+  return (
+    <div className="App">
+     <Welcom/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Welcom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from '../Styles/welcome.module.css'
+function Welcom() {
+    return (
+        <div>
+            <div className={style.welcomContainer}>
+                 <h1>Welcome ⚡</h1>
+
+
+            </div>
+           
+        </div>
+    )
+}
+
+export default Welcom
